069ab9ab7f3bdd9bcfe0ccabe7e16c6f814dd480
takuyahori <takuyahori0904@gmail.com>
2021-09-08 14:55:55 +0900

    商品情報保存機能記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+  end
+
+  private
+  
+  def item_params
+    params.require(:item).permit(:name, :explanation, :category_id, :status_id, :shipping_cost_id, :shipping_area_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+  end
 end
