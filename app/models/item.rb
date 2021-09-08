1e478f2a28c4b414d535c465f09b47c95991ba43
takuyahori <takuyahori0904@gmail.com>
2021-09-08 14:54:02 +0900

    バリデーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,24 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_cost
+  belongs_to :shipping_area
+  belongs_to :shipping_day
+
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  validates :price, format: { with: /\A[0-9]+\z/, messege: '半角数字で入力してください'}
+  validate :image_presence
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_area_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+
 end
