971f5f53cac6d09f63fc1614f1f9448a654c6795
takuyahori <takuyahori0904@gmail.com>
2021-09-10 16:03:00 +0900

    修正②

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,11 +9,14 @@ class Item < ApplicationRecord
   belongs_to :shipping_area
   belongs_to :shipping_day
 
-  validates :name, presence: true
-  validates :explanation, presence: true
-  validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  with_options presence: true do
+    validates :name
+    validates :explanation
+    validates :image
+  end
+
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   validates :price, format: { with: /\A[0-9]+\z/, messege: '半角数字で入力してください'}
-  validates :image, presence: true
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
