66b49674be2c2c96460765bfb64117c44379e384
takuyahori <takuyahori0904@gmail.com>
2021-09-22 22:35:58 +0900

    リダイレクト処理実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,13 +1,22 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    else @item.purchase.present?
+        redirect_to root_path
+    end
   end
 
   def create
     @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
+      pay_item
+      @purchase_address.save
       redirect_to root_path
     else
       render :index
@@ -17,6 +26,15 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:purchase_address).permit(:post_code, :shipping_area_id, :city, :address, :building_name, :phone).merge(user_id: current_user.id)
+    params.require(:purchase_address).permit(:post_code, :shipping_area_id, :city, :address, :building_name, :phone).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: purchase_params[:token],
+        currency: 'jpy'
+      )
   end
 end
