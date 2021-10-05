d1e25feddbd7ddbda348cee1b76c1c569150d648
takuyahori <takuyahori0904@gmail.com>
2021-10-05 18:07:34 +0900

    Revert "commit"
    This reverts commit 9e2f253ea6ae035ccd66fe2ae374d9bd405b481b.

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,10 +18,10 @@ set :ssh_options, auth_methods: ['publickey'],
                                   keys: ['~/.ssh/sample.pem'] 
 
 # プロセス番号を記載したファイルの場所
-set :unicorn_pid, -> { "#{app_path}/shared/tmp/pids/unicorn.pid" }
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
 # Unicornの設定ファイルの場所
-set :unicorn_config_path, -> { "#{app_path}/current/config/unicorn.rb" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
 # デプロイ処理が終わった後、Unicornを再起動するための記述
